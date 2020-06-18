ded0475b5c9c919a46b7e300760c576c21bb73d0
Martin Ng <martinng1992@gmail.com>
2020-06-18 16:11:24 -0400

    set up a couple components

---
+++ src/components/Jobs.js
@@ -0,0 +1,10 @@
+import React from "./node_modules/react";
+import "./App.css";
+
+function Jobs() {
+  return (
+    <div>
+      <h1>Entry Level Software Engineering Jobs</h1>
+    </div>
+  );
+}
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+/**
+ * `components/index.js` exists simply as a 'central export' for our components
+ */
+
+export { default as Jobs } from "./Jobs";
+export { default as Job } from "./Job";

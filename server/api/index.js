bebdd3f11f18cdbbf126738d0b16e63edbfdd25c
Martin Ng <martinng1992@gmail.com>
2020-06-20 02:16:52 -0400

    able to pull from redis store

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 
+const redis = require("redis");
+const client = redis.createClient();
+
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);
+
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("HELLO WORLD!!");
+});
+
+app.get("/jobs", async (req, res) => {
+  const getJobs = await getAsync("github");
+  res.send(getJobs);
 });
 
 app.listen(PORT, () => {

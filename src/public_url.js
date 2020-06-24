3d7d0d93c08c1aff26e0155c538c452ab02f1411
Martin Ng <martinng1992@gmail.com>
2020-06-24 05:49:52 -0400

    changed http into https

--- src/public_url.js
+++ src/public_url.js
@@ -1,6 +1,6 @@
 // const githubURL = "https://jobs.github.com/positions.json";
 const jobURL = "http://localhost:8000/api/jobs";
-const prodJobURL = "http://zeroyoe.heroku.app.com/api/jobs";
+const prodJobURL = "https://zeroyoe.heroku.app.com/api/jobs";
 
 module.exports = {
   JOB_URL: jobURL,

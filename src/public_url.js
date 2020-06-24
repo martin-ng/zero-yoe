099df1f56ddee77421d79410dcea13129a8b815b
Martin Ng <martinng1992@gmail.com>
2020-06-24 05:35:10 -0400

    modified variables

--- src/public_url.js
+++ src/public_url.js
@@ -1,6 +1,6 @@
 // const githubURL = "https://jobs.github.com/positions.json";
-const jobURL = "http://localhost:8000/jobs";
-const prodJobURL = "http://zeroyoe.heroku.app.com/jobs";
+const jobURL = "http://localhost:8000/api/jobs";
+const prodJobURL = "http://zeroyoe.heroku.app.com/api/jobs";
 
 module.exports = {
   JOB_URL: jobURL,

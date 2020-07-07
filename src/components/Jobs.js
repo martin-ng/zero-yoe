f93b62f885d44569422e49058836a093c5ddd7a1
Martin Ng <martinng1992@gmail.com>
2020-07-07 17:53:21 -0400

    removed pagination button

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import MobileStepper from "@material-ui/core/MobileStepper";
-import Button from "@material-ui/core/Button";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 import "./components.css";
 
 import { Job, ModalCard } from "./index";
@@ -19,20 +14,6 @@ import { Job, ModalCard } from "./index";
 export default function Jobs(props) {
   const jobsList = props.jobsList;
 
-  const numOfJobs = jobsList.length;
-  // const classes = useStyles();
-  // const theme = useTheme();
-  const [activeStep, setActiveStep] = useState(0);
-  // const [currentJob, setCurrentJob] = useState({});
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
   return (
     <div className="jobs-container">
       <div>
@@ -40,39 +21,8 @@ export default function Jobs(props) {
           {jobsList.length ? (
             jobsList.map((job, index) => <Job key={index} data={job} />)
           ) : (
-            <h1>NOTHING FOR NOW!</h1>
+            <h1>Loading Jobs!</h1>
           )}
-        </div>
-        <div className="step-button">
-          <div>
-            Page {activeStep} of {numOfJobs}
-          </div>
-          <MobileStepper
-            variant="progress"
-            steps={Math.ceil(numOfJobs / 25)}
-            position="static"
-            activeStep={activeStep}
-            nextButton={
-              <Button
-                size="small"
-                onClick={handleNext}
-                disabled={activeStep === 5}
-              >
-                Next
-                <KeyboardArrowRight />
-              </Button>
-            }
-            backButton={
-              <Button
-                size="small"
-                onClick={handleBack}
-                disabled={activeStep === 0}
-              >
-                <KeyboardArrowLeft />
-                Back
-              </Button>
-            }
-          />
         </div>
       </div>
     </div>
